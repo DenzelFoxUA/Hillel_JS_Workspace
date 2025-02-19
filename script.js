4104dbbb5a4fec89a21750013f7274471c452549
DenzelFoxUA <canemidnight21@gmail.com>
2025-02-19 14:28:27 +0200

    HW1_FirstTask

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let num = 111;
+let str = "word";
+let isTrue = false;
+let _null = null;
+let _symbol = Symbol("Symbol");
+
+
+
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(isTrue));
+console.log(typeof(_null));
+console.log(typeof(_symbol));
