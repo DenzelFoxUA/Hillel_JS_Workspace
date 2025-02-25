a41f0743394f1ee3f3c061498db5314868587ebf
DenzelFoxUA <canemidnight21@gmail.com>
2025-02-25 22:29:09 +0200

    HW5.1

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let _numberStart = 20;
+let _numberFinish = 30;
+let _stepPeriod = 0.5;
+
+while(_numberStart <= _numberFinish)
+{
+    console.log(_numberStart);
+    _numberStart += _stepPeriod;
+}
+
