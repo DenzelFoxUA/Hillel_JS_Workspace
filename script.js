733749e62b3eab292d9d6572273d4457b5c6bbe8
DenzelFoxUA <canemidnight21@gmail.com>
2025-03-08 23:32:39 +0200

    HW7.2

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var multiplyNumbers = (a) => (b) => a * b;
+
+console.log(multiplyNumbers(2)(3));
+console.log(multiplyNumbers(4)(3));
+console.log(multiplyNumbers(12)(3));
+console.log(multiplyNumbers(20)(3));
