0d57d21d3abc1f7d0b6940b41d41b316da60b80b
DenzelFoxUA <canemidnight21@gmail.com>
2025-03-24 20:27:17 +0200

    HW12.1

---
+++ script.js
@@ -0,0 +1,44 @@
+
+const inputButton = document.querySelector("#inputBtn");
+const redirectButton = document.querySelector("#redirectBtn");
+const urlStorage = document.querySelector("#urlContainer");
+
+let myUrl = "";
+
+inputButton.addEventListener("click", (e) => 
+{
+    urlStorage.innerHTML = "";
+    myUrl = "https://";
+
+    const urlInput = prompt("Enter your URL: ");
+
+    if(urlInput !== "" && urlInput !== null)
+    {
+        myUrl += urlInput;
+        alert("URL Saved");
+
+        let urlRow = document.createElement("h2");
+        urlRow.textContent = `URL: ${myUrl}`;
+        urlStorage.appendChild(urlRow);
+        console.log(myUrl);
+    }
+    else
+    {
+        alert("Empty URL is not allowed");
+        myUrl = "";
+    }
+
+    e.stopPropagation();
+});
+
+redirectButton.addEventListener("click", (e) => {
+    if(myUrl === "")
+    {
+        alert("You must add url first!");
+        
+    }
+    else
+        window.location.href = myUrl;
+        
+})
+
