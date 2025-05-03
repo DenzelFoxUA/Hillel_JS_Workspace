8c98d7400fcb6d2a3737650f71b1ed5a8ed0f356
DenzelFoxUA <canemidnight21@gmail.com>
2025-05-03 15:06:29 +0300

    HW22Webpack

--- js/postHTML_Interface.js
+++
@@ -1,6 +0,0 @@
-export const postList = document.getElementById("postList");
-export const commentForm = document.getElementById("leaveAComment");
-export const confirmBtn = document.getElementById("confirmBtn");
-export const addedPost = document.getElementById("result");
-export const input_postTitle = document.getElementById("titleInput");
-export const input_postBody = document.getElementById("bodyInput");
--- js/settingsValues.js
+++
@@ -1,4 +0,0 @@
-export const API_URL = 'https://jsonplaceholder.typicode.com';
-export const POSTS_SECTION = 'posts';
-export const QUERY_LIMIT = '?_limit=';
-export const COMMENTS_SECTION = 'comments';
